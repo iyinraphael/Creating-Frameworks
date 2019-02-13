25eb2f4811794a627bea6bf378e02ffd1a3b5bff
iyinraphael <iyinraphael@gmail.com>
2019-02-13 15:14:19 -0800

    frameWork implemented

--- UsingLoadingUI/UsingLoadingUI/ToLoadViewController.swift
+++ UsingLoadingUI/UsingLoadingUI/ToLoadViewController.swift
@@ -11,13 +11,24 @@ import ToLoadUI
 
 class ToLoadViewController: UIViewController {
     
-    var loadViewController = LoadingViewController(frame: <#T##CGRect#>)
+    let toLoadView = LoadingViewController(frame: CGRect(x: 150, y: 250, width: 100, height: 100))
     
-    let frame = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(toLoadView)
     }
+    
+    
+   
+    
+    @IBAction func start(_ sender: UIBarButtonItem) {
+       toLoadView.startAnimating()
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        toLoadView.stopAnimating()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     
 }
