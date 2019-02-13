09a61c45da84e8151c4e950623f92a9609139ffe
iyinraphael <iyinraphael@gmail.com>
2019-02-13 12:53:52 -0800

    initial commit, project framework set up

---
+++ ToLoadUI/ToLoadUI/ToLoadUI.h
@@ -0,0 +1,19 @@
+//
+//  ToLoadUI.h
+//  ToLoadUI
+//
+//  Created by Iyin Raphael on 2/13/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToLoadUI.
+FOUNDATION_EXPORT double ToLoadUIVersionNumber;
+
+//! Project version string for ToLoadUI.
+FOUNDATION_EXPORT const unsigned char ToLoadUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToLoadUI/PublicHeader.h>
+
+
