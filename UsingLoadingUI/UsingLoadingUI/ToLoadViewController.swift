b5edc51e2b4a47604b2e31582103c8b09915ebc3
iyinraphael <iyinraphael@gmail.com>
2019-02-13 14:00:01 -0800

    storyboard set up and Framework scope set

---
+++ UsingLoadingUI/UsingLoadingUI/ToLoadViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ToLoadViewController.swift
+//  UsingLoadingUI
+//
+//  Created by Iyin Raphael on 2/13/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+import ToLoadUI
+
+class ToLoadViewController: UIViewController {
+    
+    var loadViewController = LoadingViewController(frame: <#T##CGRect#>)
+    
+    let frame = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
